206366f7663c279b6b03446eb1999d68e2c0d8bf
peter88213 <petert88213@gmail.com>
2024-09-10 21:43:21 +0200

    Update the build system
    - Update docstring.
    - Remove obsolete setup script.

--- tools/build.py
+++ tools/build.py
@@ -1,10 +1,6 @@
 """Build the scap_novx application package.
         
-In order to distribute a single script without dependencies, 
-this script "inlines" all modules imported from the novxlib package.
-
-The novxlib project (see see https://github.com/peter88213/novxlib)
-must be located on the same directory level as the novelibre project. 
+Note: VERSION must be updated manually before starting this script.
 
 For further information see https://github.com/peter88213/scap_novx
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
